854028ef3c34421d3510bdf1326e16fa5f34f7cc
NoahRAlmeda <noahalmeda@gmail.com>
2020-04-26 03:13:05 -0400

    Create a post method which accept new campground entry

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 var express = require("express");
 var app = express();
 let port = 4000;
+var bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
+let campgrounds = [
+    { name: "Culver Summer Camp", image: "https://data.parkbench.com/content/data/businesses/8/e/e/a/9/NjAweDYwMA--_8eea958134562f87d68c772375cd03b2.jpg" },
+    { name: "Guajataca Summer Camp", image: "https://puertorico.com.pr/wp-content/uploads/2013/05/Guajataca-Lake-02.jpg" },
+    { name: "Nookz Summer Camp", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFf520NswwAp5Ru7kWEoaMoDuhMQhhhj8nPpqJwloX9wlPPEtY&s" }
+];
 
 app.get('/', (req, res) => {
     console.log("Entering GET landing");
@@ -13,13 +20,22 @@ app.get('/', (req, res) => {
 
 app.get('/campgrounds', (req, res) => {
     console.log("Entering GET campgrounds");
-    let campgrounds = [
-        {name: "Culver Summer Camp", image: "https://data.parkbench.com/content/data/businesses/8/e/e/a/9/NjAweDYwMA--_8eea958134562f87d68c772375cd03b2.jpg"},
-        {name: "Guajataca Summer Camp", image: "https://puertorico.com.pr/wp-content/uploads/2013/05/Guajataca-Lake-02.jpg"},
-        {name: "Nookz Summer Camp", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFf520NswwAp5Ru7kWEoaMoDuhMQhhhj8nPpqJwloX9wlPPEtY&s"}
-    ];
 
     res.render("campgrounds", {campgrounds: campgrounds});
+});
+
+app.get("/campgrounds/new", (req, res) => {
+    res.render("new");
+});
+
+app.post("/campgrounds", (req, res) => {
+    let name = req.body.name;
+    let image = req.body.image;
+    let newCampground = {name: name, image: image};
+
+    campgrounds.push(newCampground);
+
+    res.redirect("/campgrounds");
 });
 
 app.get("*", (req, res) => {
